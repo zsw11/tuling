db6dcb93cee3c5d082c9407543f8d28943a69d4b
shuwen.zhou <546232194@qq.com>
2022-01-07 11:12:30 +0800

    JMM

---
+++ javase/src/main/java/com/zsw/JMM/Jmm06_MemoryBarrier.java
@@ -0,0 +1,37 @@
+package com.yg.edu.jmm;
+
+import com.yg.edu.jmm.util.UnsafeInstance;
+
+/**
+ * ,;,,;
+ * ,;;'(    社
+ * __      ,;;' ' \   会
+ * /'  '\'~~'~' \ /'\.)  主
+ * ,;(      )    /  |.     义
+ * ,;' \    /-.,,(   ) \    码
+ * ) /       ) / )|    农
+ * ||        ||  \)
+ * (_\       (_\
+ *
+ * @author ：杨过
+ * @date ：Created in 2020/4/29
+ * @version: V1.0
+ * @slogan: 天下风云出我辈，一入代码岁月催
+ * @description:
+ **/
+public class Jmm06_MemoryBarrier {
+    int a;
+    public volatile int m1 = 1;
+    public volatile int m2 = 2;
+
+    public void readAndWrite() {
+        int i = m1;   // 第一个volatile读
+        int j = m2;   // 第二个volatile读
+
+        a = i + j;    // 普通写
+
+        m1 = i + 1;   // 第一个volatile写
+        m2 = j * 2;   // 第二个 volatile写
+    }
+
+}
