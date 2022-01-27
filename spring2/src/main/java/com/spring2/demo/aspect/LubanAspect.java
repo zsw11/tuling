69906f71048e1db5a3c153c04acdd8e6245dd5b5
shuwen.zhou <546232194@qq.com>
2022-01-27 16:18:15 +0800

    1

--- spring2/src/main/java/com/spring2/demo/aspect/LubanAspect.java
+++ spring2/src/main/java/com/spring2/demo/aspect/LubanAspect.java
@@ -2,9 +2,10 @@ package com.spring2.demo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
 @Aspect
-//@Component
+@Component
 public class LubanAspect {
 
 	@Pointcut("com.luban.aop.aspect.LubanAspect.test()")
